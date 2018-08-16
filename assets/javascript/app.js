5cd7eb43ac4bf70134224dc09d84016431a6a348
Stephen Ridings <SRidings777@aol.com>
2018-08-16 15:54:29 -0500

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -80,3 +80,14 @@ function getNextArrivalTime(minutesAway)
 
     return nextArrivalTime;
 }
+function formatAMPM(date) 
+{
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; 
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
